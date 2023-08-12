17b61f6c1e5bcdf8a39d2de8c3cc21c2f78ae944
Jajauma's GitHub <jajauma@2ch.hk>
2023-08-12 14:03:15 +0000

    Remove PS custom colors (not required any more)

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,2 @@
-# Override only some colors so they work best with vim terminal.
-Set-PSReadLineOption -Colors @{
-  Command  = "`e[35m" # Magenta
-  String   = "`e[91m" # Bright Red
-  Variable = "`e[94m" # Bright Blue
-}
-
 Set-PSReadlineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
 Set-PSReadlineKeyHandler -Chord Ctrl+u -Function BackwardKillInput
