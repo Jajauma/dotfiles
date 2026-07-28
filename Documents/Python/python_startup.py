87dfa99c5bfdf070bb8cc6615ab5a1b1b5cd56f1
Jajauma's GitHub <jajauma@2ch.hk>
2026-07-28 20:40:52 +0300

    python_startup.py: remove stale pyreadline3 import

--- Documents/Python/python_startup.py
+++ Documents/Python/python_startup.py
@@ -1,11 +1,4 @@
 import math
-import sys
-from pathlib import Path
-
-if sys.platform == "win32":
-    sys.path.append(str(Path.home() / "Documents" / "Python" / "pyreadline3.zip"))
-    import pyreadline3  # noqa: F401
-
 
 def dpi(pw: int, ph: int, idiag: float) -> float:
     return math.sqrt(pw * pw + ph * ph) / idiag
