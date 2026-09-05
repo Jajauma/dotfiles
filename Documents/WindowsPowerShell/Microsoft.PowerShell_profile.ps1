db96ae0ccb9de506027be994055515fb8d2de801
Jajauma's GitHub <jajauma@2ch.hk>
2026-09-05 21:57:44 +0300

    Add ~/.local/bin to path using registry edit

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,20 +5,3 @@ function rmrf {
   )
   Remove-Item -Recurse -Force $Target
 }
-
-function Add-PathVariable {
-  param(
-    [string]$addPath
-  )
-  if (Test-Path $addPath) {
-    $regexAddPath = [regex]::Escape($addPath)
-    $arrPath = $env:Path -split ';' | Where-Object {
-      $_ -and $_ -notmatch "^$regexAddPath\\?"
-    }
-    $env:Path = ($arrPath + $addPath) -join ';'
-  } else {
-    throw "'$addPath' is not a valid path."
-  }
-}
-
-Add-PathVariable "$HOME\.local\bin"
